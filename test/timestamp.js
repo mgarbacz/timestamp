54220b6ce4576f6b633ba131fa1632c5c096917d
Michal Garbacz <mich.garbacz@gmail.com>
2014-09-21 22:27:45 -0500

    Add test for Date constructor emulation

--- test/timestamp.js
+++ test/timestamp.js
@@ -8,4 +8,10 @@ describe('Timestamp', function() {
         timestamp = new Timestamp();
         expect(timestamp).to.be.ok;
     });
+    
+    it('should return like Date with no arguments', function() {
+       timestamp = new Timestamp();
+       var date = new Date();
+       expect(timestamp).to.equal(date);
+    });
 });
