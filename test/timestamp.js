13c24eb65d1c0f2365dadccce55e5e6259ce5687
mgarbacz <mich.garbacz@gmail.com>
2014-09-25 02:01:01 -0500

    Add test for invalid datetime attribute on <time>

--- test/timestamp.js
+++ test/timestamp.js
@@ -18,4 +18,26 @@ describe('Timestamp', function() {
 
         expect(fn).to.throw(Error, /element must be valid time element/);
     });
+
+    it('should not accept <time> element without valid datetime', function() {
+        var testElement = {};
+        testElement.nodeName = 'time';
+        testElement.attributes = {};
+
+        var fnNoDatetime = function() {
+            testElement.attributes.datetime = undefined;
+            Timestamp.format({ element: testElement });
+        };
+
+        var fnInvalidDatetime = function() {
+            testElement.attributes.datetime = 'Invalid datetime';
+            Timestamp.format({ element: testElement });
+        };
+
+        expect(fnNoDatetime).to
+            .throw(Error, /element must have a valid datetime attribute/);
+        expect(fnInvalidDatetime).to
+            .throw(Error, /element must have a valid datetime attribute/);
+
+    });
 });
