6bb03394ce15763fab2b0eceb0225a95d09b314b
mgarbacz <mich.garbacz@gmail.com>
2014-09-25 02:01:28 -0500

    Throw error on invalid datetime attribute

--- src/timestamp.js
+++ src/timestamp.js
@@ -6,7 +6,14 @@
     Timestamp.format = function(params) {
         if (params.element.nodeName !== 'time')
             throw new Error('element must be valid time element');
+        if (!isDatetime(params.element.attributes.datetime))
+            throw new Error('element must have a valid datetime attribute');
         return;
+    };
+
+    var isDatetime = function(datetime) {
+        var date = new Date(datetime);
+        return (date !== "Invalid Date" && !isNaN(date));
     };
 
     if (typeof define !== 'undefined' && define.amd) {
